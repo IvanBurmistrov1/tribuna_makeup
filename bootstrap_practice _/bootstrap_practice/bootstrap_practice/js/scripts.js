2261a1172bc47d74a28c861306d752a191e33a4c
Vasilii <ricsentrika@gmail.com>
2018-08-14 18:48:23 +0300

    commit 14.08

--- bootstrap_practice _/bootstrap_practice/bootstrap_practice/js/scripts.js
+++ bootstrap_practice _/bootstrap_practice/bootstrap_practice/js/scripts.js
@@ -13,58 +13,138 @@ $(document).ready(function () {
         width: '60px'
     });
     $("<style type='text/css' id='dynamic' />").appendTo("head");
-    $('#slider').lightSlider({
-        item: 4,
-        loop: false,
-        slideMove: 2,
-        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed: 600,
-        autoHeight:true,
+    $("#politicaonline_on_small .tab-pane").each(function (key, item) {
+        var next_selector = "#politicaonline_next_" + this.id;
+        var prev_selector = "#politicaonline_prev_" + this.id;
+        var slider_selector = "#" + this.id + " .slider-politica";
+        make_slider(slider_selector, prev_selector, next_selector);
+    });
+    $("#politica_tabs>ul>li>a").click(function () {
+        var tab_id = this.getAttribute("href");
+        var slider_selector =  tab_id + " .slider-politica";
+        var next_selector = "#politicaonline_next_" + tab_id.substr(1);
+        var prev_selector = "#politicaonline_prev_" + tab_id.substr(1);
+        reload_slider(slider_selector,next_selector,prev_selector);
+    });
+    function reload_slider(slider_selector, prev_selector, next_selector) {
+       /* var slider=$(slider_selector).reloadSlider({
+            mode: 'vertical',
+            minSlides: 5,
+            maxSlides: 5,
+            adaptiveHeight: true,
+            hideControlOnEnd: true,
+            infiniteLoop: false,
+            wrapperClass: 'bx-wrapper',
+            pager: false,
+            controls: true,
+            nextSelector: next_selector,
+            prevSelector: prev_selector,
+            nextText: '<div style="height:30px;width: 100%" ></div>',
+            prevText: '<div style="height:30px;width: 100%" ></div>',
+        });*/
+        $(window).trigger('resize');
 
+        //slider.redrawSlider();
+       // $(slider_selector).resizeWindow();
+    }
+    $('#politica_tabs>ul>li>a').on('shown.bs.tab', function(){
+        alert('The new tab is about to be shown.');
+        $(window).trigger('resize');
     });
 
-/*
-    $('.slider').slick({
-        dots: false,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        vertical: true,
-
-        adaptiveHeight: true
-    });
-
-    function onResize() {
-        if (window.matchMedia('screen and (min-width: 768px) and (max-width: 1200px)').matches) {
-            //...
-            var height = $("#blogosfera_col_on_small").height();
-            var h1 = $("#politicaonline_on_small").height();
-            var h2 = $("#blogosfera_on_small").height();
-            $("#separate_columns").height(Math.max(h1, h2) + 1);
-
-        } else {
-            //  $("#politicaonline_on_small").height("auto");
-        }
+    function make_slider(slider_selector, prev_selector, next_selector) {
+        $(slider_selector).bxSlider({
+            mode: 'vertical',
+            minSlides: 5,
+            maxSlides: 5,
+            adaptiveHeight: true,
+            hideControlOnEnd: true,
+            infiniteLoop: false,
+            wrapperClass: 'bx-wrapper',
+            pager: false,
+            controls: true,
+            nextSelector: next_selector,
+            prevSelector: prev_selector,
+            nextText: '<div style="height:30px;width: 100%" ></div>',
+            prevText: '<div style="height:30px;width: 100%" ></div>',
+        });
     }
 
-    onResize();
-    $(window).resize(function () {
+    /*
+    $('.slider-politica').bxSlider({
+        mode: 'vertical',
+        minSlides: 5,
+        maxSlides: 5,
+        adaptiveHeight: true,
+        hideControlOnEnd: true,
+        infiniteLoop: false,
+        wrapperClass: 'bx-wrapper',
+        pager: false,
+        controls: true,
+        nextSelector:'#politicaonline_next',
+        prevSelector:'#politicaonline_prew',
+        nextText: '<div style="height:30px;width: 100%" ></div>',
+        prevText: '<div style="height:30px;width: 100%" ></div>',
+    });
+
+    $('#slider-blogosfera').bxSlider({
+        mode: 'vertical',
+        minSlides: 5,
+        maxSlides: 5,
+        adaptiveHeight: true,
+        hideControlOnEnd: true,
+        infiniteLoop: false,
+        wrapperClass: 'bx-wrapper',
+        pager: false,
+        controls: true,
+        nextSelector:'#politicaonline_next',
+        prevSelector:'#politicaonline_prew',
+        nextText: '<div style="height:30px;width: 100%" ></div>',
+        prevText: '<div style="height:30px;width: 100%" ></div>',
+    });
+
+
+    /*
+        $('.slider').slick({
+            dots: false,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            vertical: true,
+
+            adaptiveHeight: true
+        });
+
+        function onResize() {
+            if (window.matchMedia('screen and (min-width: 768px) and (max-width: 1200px)').matches) {
+                //...
+                var height = $("#blogosfera_col_on_small").height();
+                var h1 = $("#politicaonline_on_small").height();
+                var h2 = $("#blogosfera_on_small").height();
+                $("#separate_columns").height(Math.max(h1, h2) + 1);
+
+            } else {
+                //  $("#politicaonline_on_small").height("auto");
+            }
+        }
+
         onResize();
-    });
+        $(window).resize(function () {
+            onResize();
+        });
 
-*/
-/*
-var $whatever        = $('#container');
-var ending_right     = ($(window).width() - ($whatever.offset().left + $whatever.outerWidth()));
-  $("#dynamic").text("#breaking_news:after{width:" + ending_right+ "px;}");
+    */
+    /*
+    var $whatever        = $('#container');
+    var ending_right     = ($(window).width() - ($whatever.offset().left + $whatever.outerWidth()));
+      $("#dynamic").text("#breaking_news:after{width:" + ending_right+ "px;}");
 
-    $('.richmedia_fisc div a').click(function(){
-        $('.richmedia_fisc').hide();
-    });
-*/
-})
-;
+        $('.richmedia_fisc div a').click(function(){
+            $('.richmedia_fisc').hide();
+        });
+    */
+});
 
 /*
 $(window).resize(function () { 
